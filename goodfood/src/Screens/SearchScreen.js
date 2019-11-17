8a0f0c166b57b0e7558a334ded64a95e87e0f789
sudir81 <papinenisudheer@yahoo.com>
2019-11-17 17:34:07 -0500

    Separated hooks into separate file, so that we can use across app.

--- goodfood/src/Screens/SearchScreen.js
+++ goodfood/src/Screens/SearchScreen.js
@@ -1,34 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import SearchBar from "./../components/SearchBar";
-import yelp from "../api/yelp";
+import useBusinessResults from "./../hooks/useBusinessResults";
 
 const SearchScreen = () => {
   const [searchText, setSearchText] = useState("");
-  const [results, setResults] = useState([]);
-  const [errMsg, setErrMsg] = useState("");
-
-  const searchAPI = async searchTxt => {
-    console.log("search APi called.");
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 50,
-          term: searchTxt,
-          location: "new york"
-        }
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      console.log(err);
-      setErrMsg("Something went wrong, try again.");
-    }
-  };
-
-  // Default search when component load
-  useEffect(() => {
-    searchAPI("thai");
-  }, []);
+  const [searchAPI, results, errMsg] = useBusinessResults();
 
   return (
     <View>
---
+++ goodfood/src/hooks/useBusinessResults.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import yelp from "../api/yelp";
+
+export default () => {
+  const [results, setResults] = useState([]);
+  const [errMsg, setErrMsg] = useState("");
+
+  const searchAPI = async searchTxt => {
+    console.log("search APi called.");
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTxt,
+          location: "new york"
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      console.log(err);
+      setErrMsg("Something went wrong, try again.");
+    }
+  };
+
+  // Default search when component load
+  useEffect(() => {
+    searchAPI("thai");
+  }, []);
+
+  return [searchAPI, results, errMsg];
+};
